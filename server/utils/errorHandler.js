379bd283a7eb491ce3392a0d03df3b7ab80a1060
Castiel <56901212+castiel01x@users.noreply.github.com>
2023-12-01 18:58:01 +0200

    Finished carts route, added centralized error handling

---
+++ server/utils/errorHandler.js
@@ -0,0 +1,7 @@
+// errorHandler.js
+module.exports = {
+    handleUnauthorized: (res) => res.status(401).json({ error: 'Unauthorized - Invalid Token' }),
+    handleServerError: (res) => res.status(500).json({ error: 'Internal Server Error' }),
+    handleUserNotFound: (res) => res.status(404).json({ error: 'User not found' }),
+    handleBadRequest: (res, message) => res.status(400).json({ error: message }),
+};
