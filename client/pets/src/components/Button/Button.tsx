55755c65daca5f6cc2b7963ad04cea6e719b000b
Castiel <56901212+castiel01x@users.noreply.github.com>
2023-12-06 23:19:15 +0200

    Frontend

---
+++ client/pets/src/components/Button/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Button} from "react-bootstrap";
+
+interface ButtonProps {
+    className?: string;
+    type?: 'button' | 'submit' | 'reset';
+    variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+    size?: 'lg' | 'sm';
+    disabled?: boolean;
+    onClick?: () => void;
+    children: React.ReactNode;
+}
+
+const ButtonComponent: React.FC<ButtonProps> = ({
+                                                    className = '',
+                                                    type = 'button',
+                                                    variant = 'primary',
+                                                    size,
+                                                    disabled = false,
+                                                    onClick,
+                                                    children
+                                                }) => {
+    const classes = `btn btn-${variant} ${size ? `btn-${size}` : ''} ${className}`;
+
+    return (
+        <Button type={type} className={classes} disabled={disabled} onClick={onClick}>
+            {children}
+        </Button>
+    );
+};
+
+export default ButtonComponent;
